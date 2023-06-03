8d49ca2e82d2dd3f9725ba92afa36d3653b8bff2
darlene503 <duyn503@gmail.com>
2023-06-02 18:57:37 -0700

    figure out ifs

--- java.js
+++ java.js
@@ -9,9 +9,13 @@ window.onload = function() {
   document.querySelector("form").onsubmit = function(event) {
     event.preventDefault();
     hideResults();
-    const age = parseInt(document.querySelector("input#age").value);
-    const height = parseInt(document.querySelector("input#height").value);
-  };
+    const age = parseInt(document.querySelector("input#number").value);
+  }
+  
+  // we're evaluating the Tower of Doom ride first
+  if (age >= 12 && height >= 60) {
+    document.getElementById("swings").removeAttribute("class");
+    document.getElementById("coaster").removeAttribute("class");
+    document.getElementById("tower").removeAttribute("class");
+  } 
 };
-
-
