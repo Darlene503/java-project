87d2b8f00db7182adcbe489d379c89bd40a32dda
darlene503 <duyn503@gmail.com>
2023-06-04 10:40:13 -0700

    cleaning up js

--- java.js
+++ java.js
@@ -1,4 +1,5 @@
 function hideResults() {
+  document.getElementById("Error").setAttribute("class", "hidden");
   document.getElementById("Javascript").setAttribute("class", "hidden");
   document.getElementById("C#").setAttribute("class", "hidden");
   document.getElementById("Python").setAttribute("class", "hidden");
@@ -10,9 +11,11 @@ window.onload = function() {
     event.preventDefault();
     hideResults();
     const times = parseInt(document.querySelector("input#number").value);
+    const camp = parseInt(document.querySelector("input#number").value);
+
   }
   
-  if (times >= 3 && height >= 10) {
+  if (times >= 3 && camp >= 10) {
     document.getElementById("Javascript").removeAttribute("class");
     document.getElementById("C#").removeAttribute("class");
     document.getElementById("Python").removeAttribute("class");
