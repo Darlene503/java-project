2dafa6634210daf2e49abc245e66f40ccbe6deba
darlene503 <duyn503@gmail.com>
2023-06-04 12:44:32 -0700

    cleaing up html answers

--- java.js
+++ java.js
@@ -15,17 +15,18 @@ window.onload = function() {
     const camp = parseInt(document.querySelector("input#camp").value);
     const fly = parseInt(document.querySelector("input#fly").value);
 
-    if (isNaN(times) && isNaN(camp) && isNaN(fly)) {
+    if (!isNaN(times) && !isNaN(camp) && !isNaN(fly)) {
       if (times >= 8 && camp >= 50 && fly >= 10) {
         document.getElementById("Javascript").removeAttribute("class");
-      } else if { (times >= 5 && camp >= 25 && fly >= 6) 
-       document.getElementById("C-sharp").removeAttribute("class");
-
-      } else if { (times >= 3 && camp >= 15 && fly >= 2);
+      } else if (times >= 5 && camp >= 25 && fly >= 6) {
+        document.getElementById("C-sharp").removeAttribute("class");
+      } else if (times >= 3 && camp >= 15 && fly >= 2) {
         document.getElementById("Python").removeAttribute("class");
-
       } else {
         document.getElementById("Error").removeAttribute("class");
       }
+    } else {
+      document.getElementById("Error").removeAttribute("class");
+    }
   };
 };
