f0a79cad3ba2a219d719ce17f4bb48c86daba2b7
darlene503 <duyn503@gmail.com>
2023-06-04 11:56:49 -0700

    adjust html questions

--- java.js
+++ java.js
@@ -1,31 +1,31 @@
 function hideResults() {
   document.getElementById("Error").setAttribute("class", "hidden");
   document.getElementById("Javascript").setAttribute("class", "hidden");
-  document.getElementById("C#").setAttribute("class", "hidden");
+  document.getElementById("C-sharp").setAttribute("class", "hidden");
   document.getElementById("Python").setAttribute("class", "hidden");
- 
 }
 
 window.onload = function() {
-  hideResults()
+  hideResults();
 
   document.querySelector("form").onsubmit = function(event) {
     event.preventDefault();
     hideResults();
-    const times = parseInt(document.querySelector("input#number").value);
-    const camp = parseInt(document.querySelector("input#number").value);
+    const times = parseInt(document.querySelector("input#times").value);
+    const camp = parseInt(document.querySelector("input#camp").value);
+    const fly = parseInt(document.querySelector("input#fly").value);
 
-  }
-  
-  if (times && camp && flying) {
-    if (age >= 12 && height >= 60) {
-      document.getElementById("times").removeAttribute("class");
-      document.getElementById("camp").removeAttribute("class");
-      document.getElementById("fly").removeAttribute("class");
-    } else if (age >= 12 || height >= 48) {
-      document.getElementById("rafting").removeAttribute("class");
-      document.getElementById("hiking").removeAttribute("class");
-    }
-  } else (isNaN(times) && isNan(camp) && isNaN(fly)) 
-    document.getElementById("error").removeAttribute("class");
-  }; 
+    if (isNaN(times) && isNaN(camp) && isNaN(fly)) {
+      if (times >= 8 && camp >= 50 && fly >= 10) {
+        document.getElementById("Javascript").removeAttribute("class");
+      } else if { (times >= 5 && camp >= 25 && fly >= 6) 
+       document.getElementById("C-sharp").removeAttribute("class");
+
+      } else if { (times >= 3 && camp >= 15 && fly >= 2);
+        document.getElementById("Python").removeAttribute("class");
+
+      } else {
+        document.getElementById("Error").removeAttribute("class");
+      }
+  };
+};
